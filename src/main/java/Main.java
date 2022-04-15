7600f12be0aa9e4f6ff771154ca5abe76a4f5577
이준희 <junheui27@hanmail.net>
2022-04-15 15:50:52 +0900

    ReadInputTest case 생성 및 함수 구현(리팩토링 전 push)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,49 +2,59 @@ import manager.EmployeeManager;
 import model.UserRequest;
 import model.UserRequestConverter;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
     public static EmployeeManager manager = new EmployeeManager();
+    private String loadPath ="";
+    private String SavePath ="";
 
     public static void main(String arg[]) throws IOException {
         System.out.println("start");
 
+        // loadPath = arg[0];
+        // savePath = arg[1];
         // input.txt -> arg[0]
         ArrayList<String> inputs = readInput("input.txt");
-        run(inputs);
+        ArrayList<String> outputs = run(inputs);
+        writeOutput("output.txt",outputs);
 
         System.out.println("finish");
     }
 
-    public static void run(ArrayList<String> inputs) {
+    public static ArrayList<String> run(ArrayList<String> inputs){
 
         for (String input : inputs){
             UserRequest request = UserRequestConverter.convert(input);
             manager.process(request);
-
-            System.out.println(input);
         }
+        return null;
     }
 
     public static ArrayList<String> readInput(String str) throws IOException {
 
-        ArrayList<String> inputarray = new ArrayList();
-        BufferedReader br = new BufferedReader(new FileReader("C:/Users/junheui/IdeaProjects/Creativity_New/src/"+str));
+        ArrayList<String> inputArray = new ArrayList();
+        BufferedReader br = new BufferedReader(new FileReader("./"+str));
         String str2 = "";
 
         while((str2 = br.readLine())!=null){
-            inputarray.add(str2);
-
+            inputArray.add(str2);
         }
         br.close();
 
-        return inputarray;
+        return inputArray;
+    }
+
+    public static  void writeOutput(String str, ArrayList<String> outputs) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter("./" + str));
+
+        for(String str2 : outputs){
+            bw.write(str2);
+        }
+        bw.flush();
+        bw.close();
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -14,11 +14,51 @@ class MainTest {
         result.add("ADD,,,,99000101,A LEE,CL3,010-1234-5678,19920101,PRO");
         result.add("ADD,,,,99100201,B KIM,CL3,010-2341-5678,19910202,ADV");
         result.add("ADD,,,,00000301,C CHOI,CL2,010-3412-5678,19940403,EX");
-        result.add("ADD,,,,18000301,AB SEO,CL2,010-4123-5678,19981231,PRO");
-        result.add("ADD,,,,21000301,BC KANG,CL1,010-1234-6785,19990111,ADV");
+        result.add("ADD,,,,18000301,D SEO,CL2,010-4123-5678,19991231,PRO");
+        result.add("ADD,,,,21000301,E KANG,CL1,010-1234-6785,19990111,ADV");
+        result.add("ADD,,,,99000102,AA LEE,CL4,010-1234-5679,19930101,EX");
+        result.add("ADD,,,,99100202,BB KIM,CL4,010-2341-5679,19920202,PRO");
+        result.add("ADD,,,,00000302,CC CHOI,CL3,010-3412-5679,19950403,ADV");
+        result.add("ADD,,,,18000302,DD SEO,CL2,010-4123-5679,19991231,PRO");
+        result.add("ADD,,,,21000302,EE KANG,CL1,010-1234-6789,20000111,ADV");
+        result.add("ADD,,,,09000302,FF MOON,CL1,010-1234-6666,19901131,ADV");
+        result.add("SCH,-p,,,employeeNum,00000301");
+        result.add("DEL,-p,,,employeeNum,00000301");
+        result.add("SCH,-p,,,employeeNum,00000301");
+        result.add("MOD,-p,,,employeeNum,00000302,phoneNum,010-1111-2222");
+        result.add("SCH,,,,birthday,19991231");
+        result.add("DEL,,,,birthday,19991231");
+        result.add("MOD,-p,,,cl,CL1,cl,CL2");
+        result.add("DEL,-p,-f,name,AA");
+        result.add("MOD,-p,-l,phoneNum,2222,certi,PRO");
+        result.add("SCH,-p,-y,birthday,1992");
 
         for(int i =0 ; i < array.size(); i++){
             assertEquals(array.get(i), result.get(i));
+        }
+    }
+
+    @Test
+    void writeOutputTest() throws IOException {
+        ArrayList<String> array = Main.readInput("input.txt");
+        ArrayList<String> outputs = Main.run(array);
+        ArrayList<String> result = new ArrayList();
+        result.add("SCH,00000301,C CHOI,CL2,010-3412-5678,19940403,EX");
+        result.add("DEL,00000301,C CHOI,CL2,010-3412-5678,19940403,EX");
+        result.add("SCH,NONE");
+        result.add("MOD,00000302,CC CHOI,CL3,010-3412-5679,19950403,ADV");
+        result.add("SCH,2");
+        result.add("DEL,2");
+        result.add("MOD,09000302,FF MOON,CL1,010-1234-6666,19901131,ADV");
+        result.add("MOD,21000301,E KANG,CL1,010-1234-6785,19990111,ADV");
+        result.add("MOD,21000302,EE KANG,CL1,010-1234-6789,20000111,ADV");
+        result.add("DEL,99000102,AA LEE,CL4,010-1234-5679,19930101,EX");
+        result.add("MOD,00000302,CC CHOI,CL3,010-1111-2222,19950403,ADV");
+        result.add("SCH,99000101,A LEE,CL3,010-1234-5678,19920101,PRO");
+        result.add("SCH,99100202,BB KIM,CL4,010-2341-5679,19920202,PRO");
+
+        for(int i =0 ; i < outputs.size(); i++){
+            assertEquals(outputs.get(i), result.get(i));
         }
     }
 }
