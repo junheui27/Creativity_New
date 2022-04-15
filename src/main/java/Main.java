fe01568fc47d5edcdc1c3ec0cd45e97fd58b9762
이준희 <junheui27@hanmail.net>
2022-04-15 12:36:57 +0900

    Main Test 생성

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import model.UserRequest;
 import model.UserRequestConverter;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -15,6 +16,7 @@ public class Main {
     public static void main(String arg[]) throws IOException {
         System.out.println("start");
 
+
         List<String> inputs = readInput();
         run(inputs);
 
@@ -26,11 +28,19 @@ public class Main {
         for (String input : inputs){
             UserRequest request = UserRequestConverter.convert(input);
             manager.process(request);
+
+            System.out.println(input);
         }
     }
 
     private static List<String> readInput() throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+        BufferedReader br = new BufferedReader(new FileReader("C:/Users/junheui/IdeaProjects/Creativity_New/src/input.txt"));
+
+        String str;
+
+        while((str = br.readLine())!=null){
+            System.out.println(str);
+        }
         br.close();
 
         return new ArrayList<String>();
