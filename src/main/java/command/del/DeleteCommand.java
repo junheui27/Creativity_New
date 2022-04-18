2321729715381ee4d0527f21b4e8769a408b1dab
bw2218 <82104205+bw2218@users.noreply.github.com>
2022-04-18 18:48:06 +0900

    DeleteCommand, DeleteCommandTest 1차 Commit & Merge 진행

--- src/main/java/command/del/DeleteCommand.java
+++ src/main/java/command/del/DeleteCommand.java
@@ -1,20 +1,46 @@
 package command.del;
 
 import command.CommandExecutor;
+import command.sch.SearchCommand;
 import database.EmployeeDB;
 import model.Employee;
 import model.UserRequest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DeleteCommand implements CommandExecutor {
-    final private CommandExecutor schCommand;
-    public DeleteCommand(CommandExecutor schCommand) {
-        this.schCommand = schCommand;
-    }
+	final private CommandExecutor schCommand;
 
-    @Override
+	public DeleteCommand(CommandExecutor schCommand) {
+		this.schCommand = schCommand;
+	}
+
+	@Override
     public List<Employee> run(UserRequest request, EmployeeDB db) {
-        return null;
+		/*        
+         DEL, , , ,employeeNum,18115040
+		 DEL,-p,-l, ,name,MPOSXU 
+         
+		 * 
+		 * String printOption = options.get(0); // -p String nameOption =
+		 * options.get(1); // -f : 성명의 이름으로, -l : 성명의 성, -m : 전화번호 중간자리 -l 전화번호 뒷자리
+		 */
+		
+		
+    	List<Employee> employeeList = new ArrayList<Employee>();
+		try {
+			employeeList = schCommand.run(request, db);
+		} catch (Exception e) {
+			System.out.println("DeleteCommand.run() Exception 발생");
+			e.printStackTrace();
+		}
+        
+    	for(int i = 0 ; i < employeeList.size(); i++) {
+    		db.deleteEmployee(employeeList.get(i));
+    	}
+    
+    	
+    	return employeeList;
     }
 }
--- src/test/java/command/del/DeleteCommandTest.java
+++ src/test/java/command/del/DeleteCommandTest.java
@@ -1,14 +1,74 @@
 package command.del;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class DeleteCommandTest {
+import command.CommandExecutor;
+import command.add.AddCommand;
+import command.sch.SearchCommand;
+import database.EmployeeDB;
+import model.CAREERLEVEL;
+import model.COMMAND;
+import model.Employee;
+import model.UserRequest;
 
+class DeleteCommandTest {
+	CommandExecutor commandExecutor;
+	SearchCommand searchCommand;
+	EmployeeDB employeeDB;
+	@BeforeEach
+	public void initialise() {
+		if(searchCommand == null)
+			searchCommand = mock(SearchCommand.class);
+		if(commandExecutor==null)
+			commandExecutor = new DeleteCommand(searchCommand);
+		if(employeeDB==null)
+			employeeDB = new EmployeeDB();
+	}
+	
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("DeleteCommandTest 성공(Mock객체활용)") 
+	void runTestSuccess() {
+		UserRequest ur = new UserRequest();
+		ArrayList<String> ArgumentList = new ArrayList();
+		ArgumentList.add("cl");
+		ArgumentList.add("CL3");
+		
+		COMMAND command = null;
+		command = COMMAND.DEL;
+		List<String> optionList = new ArrayList();
+		
+		ur.setCommand(command);
+		ur.setOptions(optionList);
+		ur.setArguments(ArgumentList);
+		
+		List<Employee> employeeListTemp = new ArrayList<Employee>();
+		Employee employeeTemp = new Employee();
+		employeeTemp.setCl(CAREERLEVEL.CL3);
+		employeeListTemp.add(employeeTemp);
+		try {
+			when(commandExecutor.run(ur, employeeDB)).thenReturn(employeeListTemp);
+			List<Employee> employee = commandExecutor.run(ur, employeeDB);
+			assertEquals(employee.get(0).getCl(), CAREERLEVEL.CL3);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	@DisplayName("DeleteCommandTest 성공2(실제 SearchCommand 객체 활용)")
+	void runTestSucces2() {
+		
 	}
 
 }
