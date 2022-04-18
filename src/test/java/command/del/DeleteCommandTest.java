4105b70c2ab1e1082c43db9cadf8db6e2c39643c
bw2218 <82104205+bw2218@users.noreply.github.com>
2022-04-18 15:58:12 +0900

    DeleteCommandTest, ModifyCommandTest 파일 추가(빈파일)

---
+++ src/test/java/command/del/DeleteCommandTest.java
@@ -0,0 +1,14 @@
+package command.del;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DeleteCommandTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/command/mod/ModifyCommandTest.java
@@ -0,0 +1,14 @@
+package command.mod;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ModifyCommandTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
