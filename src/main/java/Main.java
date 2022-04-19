71f23c5131a527ec98a5d80e215856d2442c8956
dg1.lee <akxkdldhtm@gmail.com>
2022-04-19 11:11:48 +0900

    [refactoring] main함수 exception처리 정리

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,6 @@
 import manager.EmployeeManager;
-import model.UserRequest;
-import model.UserRequestConverter;
 
 import java.io.*;
-import java.nio.file.InvalidPathException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -14,34 +11,30 @@ public class Main {
     private static String loadPath ="";
     private static String SavePath ="";
 
-    public static void main(String arg[]) throws IOException {
-        System.out.println("start");
-
+    public static void main(String arg[]) {
         loadPath = arg[0];
         SavePath = arg[1];
         List<String> inputs = new ArrayList<String>();
-
-        try{
-            isValidInputFileExistTrue(loadPath);
-            inputs = readInput(loadPath);
-            run(inputs,SavePath);
-
-
-
-        }catch(Exception e) {
-            System.out.println(e);
+        
+        if(!isValidInputFileExistTrue(loadPath)){
+            System.out.println("no file");
+            return;
         }
 
-        System.out.println("finish");
+        inputs = readInput(loadPath);
+        if(inputs == null || inputs.size() == 0){
+            System.out.println("wrong file");
+            return;
+        }
+
+        List<String> outputs = run(inputs);
+        writeOutput("SavePath",outputs);
     }
     
 
-    static boolean isValidInputFileExistTrue(String path) throws Exception {
-    	File file = new File("./"+path);
-
-        if(file.exists()==false){throw new FileNotFoundException(" file not exists");}
-    	if(path.indexOf('\0')!=-1){throw new InvalidPathException(path," file not exists");}
-    	return true;
+    static boolean isValidInputFileExistTrue(String str) {
+    	File file = new File("./"+str); 
+    	return file.exists();
     }
 
 
@@ -51,29 +44,62 @@ public class Main {
     	return Pattern.matches(regx, str);
     }
 
-    public static void run(List<String> inputs,String savePath) throws Exception {
 
-        for (String input : inputs){
-            UserRequest request = UserRequestConverter.convert(input);
-            request.setOutputPath(savePath);
-            manager.process(request);
-        }
-
+    static boolean isValidOutputFormatTrue(String str) {
+    	String regx = "^[A-Z]{3}\\,.*";
+    	return Pattern.matches(regx, str);
     }
 
-    public static List<String> readInput(String str) throws IOException {
+    public static List<String> run(List<String> inputs){
+
+        for (String input : inputs){
+            //UserRequest request = UserRequestConverter.convert(input);
+            //manager.process(request);
+        }
+        return inputs;
+    }
+
+    public static List<String> readInput(String str){
 
         List<String> inputArray = new ArrayList();
-        BufferedReader br = new BufferedReader(new FileReader("./"+str));
-        String str2 = "";
-
-        while((str2 = br.readLine())!=null){
-            if(isValidInputFormatTrue(str2))
-            	inputArray.add(str2);
+        BufferedReader br = null;
+        try{
+            br = new BufferedReader(new FileReader("./"+str));
         }
-        br.close();
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+            return inputArray;
+        }
+
+        try{
+            String str2 = "";
+
+            while((str2 = br.readLine())!=null){
+                if(isValidInputFormatTrue(str2))
+                    inputArray.add(str2);
+            }
+            br.close();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
 
         return inputArray;
     }
 
+    public static  void writeOutput(String str, List<String> outputs) {
+
+        try{
+            BufferedWriter bw = new BufferedWriter(new FileWriter("./" + str));
+
+            for(String str2 : outputs){
+                bw.write(str2 + "\n");
+            }
+            bw.flush();
+            bw.close();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 }
